cf1bac2346c6e86c08d1501a2e055c0ae2ad06c0
seandcoleman <coleman.sd@gmail.com>
2016-02-26 17:00:58 -0800

    Footer and Header Directives

---
+++ src/js/controllers/chooseLangCtrl.js
@@ -0,0 +1,27 @@
+(function() {
+    "use strict";
+    angular.module('spring-2885')   
+        .controller('chooseLangCtrl', ['$scope', function($scope){
+            
+            //$scope.storage = $localStorage
+            $scope.chooseLang = function(langKey) {
+              switch(langKey) {
+                  case 'zh_CN':
+                      break;
+                  case 'en_US':
+                      break;
+                  case 'fr_FR':
+                      break;
+                 // case 'ar_AB':
+                //      break;
+                  default:
+                      langKey = "en_US";
+                      break;
+              }
+                $translate.use(langKey);
+                //$scope.storage.profile.myLanguage = langKey;
+                //tmhDynamicLocale.set(langKet.toLowerCase().replace(/_/g, /'-')); //i18 filenames are lowercase
+            };
+    }]);
+})();
+    
