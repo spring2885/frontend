92dfec345f6a844effb8feb4a5d14474b7bd33fb
Rob Clevenger <rob@spring-2885.org>
2016-04-07 21:40:01 +0200

    Add support for creating new users from the /login page. Also added the /newuser proxy to grunt so the post to /newuser works.
    Fixes https://github.com/spring2885/frontend/issues/87

--- src/js/controllers/signupCtrl.js
+++ src/js/controllers/signupCtrl.js
@@ -1,8 +1,27 @@
 (function() {
     "use strict";
     angular.module('spring-2885')   
-        .controller('signUpCtrl', ['$scope', '$state', function($scope, $state){
+        .controller('signUpCtrl', ['$scope', '$state', '$http', 
+        function($scope, $state, $http) {
+       console.log("signUpCtrl");
            
+        $scope.formData = {};
+
+        $scope.signup = function() {
+        	$http({
+			method  : 'POST',
+			url     : 'newuser',
+			data    : $.param($scope.formData),
+			headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  
+			}).then(function successCallback(response) {
+                console.log("Newuser succeeded " + JSON.stringify(response));
+                $state.go('newsfeed-index');
+            }, function errorCallback(response) {
+                $scope.isLoggedIn = false;
+                console.log("Newuser failed " + JSON.stringify(response));
+                $state.go('login');
+            });
+        };
             
     }]);
 })();
