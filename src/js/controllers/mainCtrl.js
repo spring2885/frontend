5ea8aa5e5a4a781c149ba2ea7370ee0fbd6791f2
Rob Clevenger <rob@spring-2885.org>
2016-04-08 11:41:34 +0200

    Commit file missing, this fixes auth.

--- src/js/controllers/mainCtrl.js
+++ src/js/controllers/mainCtrl.js
@@ -5,26 +5,47 @@
                                 '$stateParams', '$localStorage', '$http', 'MessageService', '$translate', 'authDefaults', function($scope, $state, authService, $rootScope, $stateParams, $localStorage, $http, MessageService, $translate, authDefaults) {
             
                                     
-            authDefaults.authenticateUrl = '/user';
-                                    
             $scope.$storage = $localStorage;
+            if (!$scope.$storage.isLoggedIn) {
+                $scope.$storage.isLoggedIn = false;
+            }
             MessageService.configure({disabled:false, max:3, timeout:3500});
+
+            authDefaults.authenticateUrl = '/user';
+            authService.addEndpoint();
+
+            // listen for login events
+            $rootScope.$on('login', function() {
+                console.log("login event handler.");
+                $scope.loggedInUsername = authService.username();
+            });
+
+            // listen for logout events
+            $rootScope.$on('logout', function() {
+                console.log("logout event handler.");
+                delete $scope.$storage.user;
+                $scope.$storage.isLoggedIn = false;
+                $scope.isLoggedIn = false;
+            });
                                     
-            var isLoggedIn = function() {
-                
-                var user = $localStorage.user;
-                if (!user) {
-                    
-                    /* for debugging:
-                    console.log("isLoggedIn: false");
-                    */
-                    
+            var setLoggedInUser = function() {
+                $http.get('user').success(function(data) {
+                    console.log("setLoggedInUser: [/user] person: " +
+                        JSON.stringify(data.person));
+                    $scope.$storage.user = data.person;
+                });
+            };
+
+            var checkLoggedIn = function() {
+                if (!$scope.$storage.user) {
                     return false;
                 }
+                return true;
             };
                                     
-            $scope.$storage.isLoggedIn = isLoggedIn();
+            $scope.$storage.isLoggedIn = checkLoggedIn();
             $scope.isLoggedIn = $scope.$storage.isLoggedIn;
+            console.log("isLoggedIn = " + $scope.isLoggedIn);
             
             $scope.credentials = {
                 username : "",
@@ -35,20 +56,19 @@
                 authService
                     .login($scope.credentials.username, $scope.credentials.password)
                     .success(function() {
+                        console.log("/user success: " + authService.username());
+
                         $scope.$storage.isLoggedIn = true;
                         $scope.isLoggedIn = true;
-                    
-                         //TODO: Figure out how to grab Current User Profile
-                        //$scope.$storage.user = $scope.user;
-                    
-                        /*
-                        for debugging:
-                        console.log('SUCCESS: user:' + $scope.user);
-                        */
+
+                        // Grab the current user profile.
+                        setLoggedInUser();
+                        //console.log("SUCCESS: user: " + $scope.$storage.user.email);
                     
                         $state.go('newsfeed-index');
                     })
                     .error(function() {
+                        console.log("FAILURE: login failed: " + $scope.credentials.username);
                     
                         /*
                         for debugging:
@@ -62,7 +82,6 @@
             
             $scope.logout = function() {
                 authService.logout();
-                delete $localStorage.user;
                 $state.go('login');
             };
             
