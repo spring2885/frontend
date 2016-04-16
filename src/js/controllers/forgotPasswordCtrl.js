9b3fb69abecaa01bc322c74ab552780186ab58a3
Yrel <yjmac26@gmail.com>
2016-04-15 19:08:41 -0700

    forgotPass controller

---
+++ src/js/controllers/forgotPasswordCtrl.js
@@ -0,0 +1,19 @@
+(function() {
+    "use strict";
+    angular.module('spring-2885')
+        .controller('forgotPasswordCtrl', ['$http', '$scope', '$state' function($http, $scope, $state){
+                  $scope.resetPassEmail = {};
+                  var forgotData = {
+                        email : $scope.resetPassEmail;
+                  };
+                  $scope.SendEmail = function (){
+                      $http.post('/auth/forgot', forgotData)
+                         .success(function(response){
+                             $state.go('reset-password');
+                         })
+                         .error(function(response){
+                             console.log("Error: " + JSON.stringify(response));
+                         });
+                  };
+    }]);
+})();
