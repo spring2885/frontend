e14f0b723a10d4c51a02fd8fb28e6df2b69a8683
scoleman <scoleman@admins-MacBook-Pro.local>
2016-04-16 20:13:57 -0700

    Fixed Sign up, now logs in and takes you to Profile Edit.

--- src/js/controllers/signupCtrl.js
+++ src/js/controllers/signupCtrl.js
@@ -1,22 +1,34 @@
 (function() {
     "use strict";
     angular.module('spring-2885')   
-        .controller('signUpCtrl', ['$scope', '$state', '$http', 
-        function($scope, $state, $http) {
+        .controller('signUpCtrl', ['$scope', '$state', '$http', '$localStorage', 'authService', 'authDefaults', function($scope, $state, $http, $localStorage, authService, authDefaults) {
        console.log("signUpCtrl");
            
         $scope.formData = {};
+        console.log('isLoggedIn ' + $localStorage.isLoggedIn);
+        
+        function getAuth(credentials) {
+            authService
+                .login(credentials.email, credentials.password)
+                 .error(function() {
+                        console.log("FAILURE: login failed: " + $scope.credentials.username);
+                        var msg = $translate.instant('login.FAILED');
+                        MessageService.broadcast(msg, {color: 'danger'});
+                });
+        };
 
         $scope.signup = function() {
-        	console.log("signup()");
+
         	$http({
 			method  : 'POST',
 			url     : 'newuser',
 			data    : $.param($scope.formData),
 			headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  
 			}).then(function successCallback(response) {
+                console.log($scope.formData);
+                getAuth($scope.formData)
                 console.log("Newuser succeeded " + JSON.stringify(response));
-                $state.go('newsfeed-index');
+                $state.go('profile-edit');
             }, function errorCallback(response) {
                 $scope.isLoggedIn = false;
                 console.log("Newuser failed " + JSON.stringify(response));
@@ -26,3 +38,14 @@
             
     }]);
 })();
+
+/////////////////
+//$scope.login = function() {
+//                authService
+//                    .login($scope.credentials.username, $scope.credentials.password)
+//                    .error(function() {
+//                        console.log("FAILURE: login failed: " + $scope.credentials.username);
+//                        var msg = $translate.instant('login.FAILED');
+//                        MessageService.broadcast(msg, {color: 'danger'});
+//                    });
+//            };
