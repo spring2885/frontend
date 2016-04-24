07ef234536d608eed52e289b5636502d9436dfe0
scoleman <scoleman@admins-MacBook-Pro.local>
2016-04-24 15:11:47 -0700

    Fixed Job Formating

--- src/js/controllers/jobsMyPostsCtrl.js
+++ src/js/controllers/jobsMyPostsCtrl.js
@@ -1,3 +1,23 @@
-Unstaged changes after reset:
-M	src/js/controllers/jobsCreatePostCtrl.js
-M	src/js/controllers/jobsMyPostsCtrl.js
+(function() {
+    "use strict";
+    angular.module('spring-2885')   
+        .controller('jobsMyPostsCtrl', ['$scope', '$stateParams', '$http', '$state', '$localStorage', function($scope, $stateParams, $http, $state, $localStorage){
+            
+                 $scope.jobs = [];
+                 $scope.$storage = $localStorage;
+                 $scope.flag = function(id) {
+                    abuseService.abuse(id, "JOB", "");
+                 };
+
+                 $http.get('/api/v1/jobs')
+                     .success(
+                      function(response){
+                          $scope.jobs = response;
+                          return $scope.jobs;
+                      })
+                    .error(
+                     function(response){
+                         $state.go('404');
+                     });
+    }]);
+})();
