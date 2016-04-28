358a43ef8ebedbb89832b2a2a9a60da62d1bf60f
cdeoliviera <cdeoliviera@gmail.com>
2016-04-28 13:13:57 -0700

    ProfileView Formatting Changes

--- src/js/controllers/profileShowCtrl.js
+++ src/js/controllers/profileShowCtrl.js
@@ -1,25 +1,17 @@
 (function() {
-   "use strict";
-   angular.module('spring-2885')   
-       .controller('profileShowCtrl', ['$scope', '$stateParams', '$http', '$state', function($scope, $stateParams, $http, $state){
-                $scope.profile = {};
-                $http.get('/api/v1/profiles/' + $stateParams.id)
-                    .success(
+    "use strict";
+    angular.module('spring-2885')   
+        .controller('profileShowCtrl', ['$scope', '$stateParams', '$http', '$state', function($scope, $stateParams, $http, $state){
+                 $scope.profile = {};
+                 $http.get('/api/v1/profiles/' + $stateParams.id)
+                     .success(
+                      function(response){
+                          $scope.profile = response;
+                          return $scope.profile;
+                      })
+                    .error(
                      function(response){
-<<<<<<< HEAD
-                         $scope.profile = response;
-                         console.log(JSON.stringify($scope.profile));
-                         return $scope.profile;
-                     })
-                   .error(
-                    function(response){
-                        $state.go('404');
-                    });
-   }]);
-})();
-=======
                          $state.go('404');
                      });
     }]);
 })();
->>>>>>> fdaedad38569938eadaee2c4ce0bb3ede01fb725
