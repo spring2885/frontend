fe41413264a31edc087d5fca98c8cf3502fd5aca
Chris McAfee <chr.mcafee@gmail.com>
2017-03-18 15:44:37 -0700

    changed wells to panels. this helps ease transition to bs4 cards

--- src/js/controllers/profileIndexCtrl.js
+++ src/js/controllers/profileIndexCtrl.js
@@ -2,6 +2,7 @@
     "use strict";
     angular.module('spring-2885')   
         .controller('profileIndexCtrl', ['$scope', '$http', '$state', 'abuseService', function($scope, $http, $state, abuseService){
+
             $scope.profiles = [];
             $http.get('/api/v1/profiles')
                 .success(
@@ -13,19 +14,18 @@
                  function(response){
                     $state.go('404');
                 });
-            
+
             $scope.flag = function(id) {
                 abuseService.abuse(id, 'PROFILE', '');
             };
-            
-            
-            /*Show and Hide Filtered Lists */
+
             $scope.noFilter = true;
             $scope.studentFilter = false;
             $scope.alumniFilter = false;
             $scope.facultyFilter = false;
             $scope.classOfFilter = false;
             $scope.majorFilter = false;
+
             $scope.filterProfiles  = function(type){
                 if (type === 'all'){
                     $scope.noFilter = true;
@@ -76,7 +76,6 @@
                     $scope.majorFilter = true;
                 }
             };
-        
     }]);
 })();
 
